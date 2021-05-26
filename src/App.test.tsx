bc0cc6b4eb47c3aff579071b0e974f4c76c1cc1b
Syed Areeb <syedraeebshah806@gmail.com>
2021-05-26 10:50:03 +0500

    final try

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/finaly/i);
+  const linkElement = screen.getByText(/Allah/i);
   expect(linkElement).toBeInTheDocument();
 });
